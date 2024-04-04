4b0b09044fd89eec1ec7b52581815876812dbd73
Davi Fonseca <davi.fonseca.1405489@sga.pucminas.br>
2024-04-03 23:56:46 -0300

    adiciona pequenas validacoes e documentacao dos metodos

--- codigo/RestauranteAtomo/Program.cs
+++ codigo/RestauranteAtomo/Program.cs
@@ -3,39 +3,69 @@ using System.Text;
 
 namespace RestauranteAtomo
 {
-    class Program
+    internal class Program
     {
+        #region atributo estático - restaurante
         static Restaurante restaurante = new Restaurante(1);
+        #endregion
 
+        #region metodos auxiliares e de execucao dos processos
+        /// <summary>
+        /// Retorna as opcoes de menu do sistema.
+        /// </summary>
         public static String menu()
         {
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("Menu");
             sb.AppendLine("1) Atender novo cliente");
-            sb.AppendLine("4) Sair do programa");
+            sb.AppendLine("0) Sair do programa");
 
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Cria um novo cliente a partir dos dados solicitados.
+        /// </summary>
+        /// <returns>O cliente cadastrado, caso os dados sejam validos.</returns>
         public static Cliente registrarCliente()
         {
-            Cliente cliente = new Cliente();
             Console.WriteLine("Informe o nome do cliente: ");
-            cliente.Nome = Console.ReadLine();
+            string nome = Console.ReadLine();
 
             Console.WriteLine("Informe o contato do cliente (telefone)");
-            cliente.Contato = Console.ReadLine();
+            string contato = Console.ReadLine();
 
-            return cliente;
+            if(nome.Length <= 3 && contato.Length == 0)
+                return null;
+     
+            return new Cliente();
         }
 
-        public static void atenderRequisicaoCliente(Cliente c)
+        /// <summary>
+        /// Inicia o atendimento a um cliente do restaurante. Solicita a
+        /// quantidade de pessoas que vao comer no restaurante e encaminha
+        /// para a criacao e atendimento da requisicao.
+        /// </summary>
+        /// <param name="c">O cliente cuja requisicao sera criada e atendida</param>
+        public static void iniciarAtendimento(Cliente cliente)
         {
             Console.WriteLine("Reserva para quantas pessoas ?");
-            int quantidadePessoas = int.Parse(Console.ReadLine());
-            c.fazerRequisicao(quantidadePessoas);
-            restaurante.atenderCliente(c, quantidadePessoas);
+            int quantidadePessoas;
+            do
+            {
+                quantidadePessoas = int.Parse(Console.ReadLine());
+                if (quantidadePessoas <= 0)
+                {
+                    Console.WriteLine("Quantidade de pessoas deve ser maior que 0. Digite novamente");
+                }
+                else
+                {
+                   cliente.fazerRequisicao(quantidadePessoas);
+                   restaurante.atenderCliente(cliente);
+                }
+            }while (quantidadePessoas <= 0);
         }
+        #endregion
 
         static void Main(string[] args)
         {
@@ -49,17 +79,20 @@ namespace RestauranteAtomo
                 {
                     case 1:
                         Cliente cliente = registrarCliente();
-                        atenderRequisicaoCliente(cliente);
+
+                        if(cliente != null){
+                            iniciarAtendimento(cliente);
+                        }else {
+                            Console.WriteLine("Cliente não registrado. Informações não preenchidas corretamente!");
+                        }
                         break;
-                    case 2:
-                        break;
-                    case 4: 
+                    case 0:
                         break;
                     default: 
-                        Console.WriteLine("Opção inválida!");
+                        Console.WriteLine("Opção inválida!\n");
                         break;
                 }
-            } while (opcao != 4);
+            } while (opcao != 0);
         }
     }
 }
