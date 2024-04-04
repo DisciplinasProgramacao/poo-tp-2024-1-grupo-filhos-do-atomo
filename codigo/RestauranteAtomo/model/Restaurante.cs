b8c2c6946aa4291f04fe695a2371a946c4c0183d
Migge21 <128915017+MiguelInacio23@users.noreply.github.com>
2024-04-04 00:40:39 -0300

    ajuste metodos
    - Metodos adicionarFilaEspera e adicionarMesa funcionando

--- codigo/RestauranteAtomo/model/Restaurante.cs
+++ codigo/RestauranteAtomo/model/Restaurante.cs
@@ -13,7 +13,7 @@ namespace RestauranteAtomo.model
 
 
         private const int _maxMesas = 10;
-        private List<Mesas> _mesas;
+        private List<Mesa> _mesas;
         private Queue<Requisicao> _filaDeEspera;
         private int _id;
 
@@ -24,7 +24,7 @@ namespace RestauranteAtomo.model
         public Restaurante(int id)
         {
             _id = id;
-            _mesas = new List<Mesas>();
+            _mesas = new List<Mesa>();
             _filaDeEspera = new Queue<Requisicao>();
         }
 
@@ -33,17 +33,66 @@ namespace RestauranteAtomo.model
 
         #region  /* Métodos Privados */
 
-        private List<Mesas> buscarMesasLivres() { return _mesas; } //So para nao dar erro de falta de retorno
+        private List<Mesa> buscarMesasLivres() 
+        {
+           return _mesas;
+        }
+
+        private void alocarMesa(Requisicao requisicao)
+        {
+            foreach (Mesa mesas in _mesas)
+            {
+                if (mesas.validaAlocao(requisicao.QuantLugares))
+                {
+                    mesas.ocupar();
+                }
+                else
+                {
+                    adicionarFilaEspera(requisicao);
+                }
+            }
+        }
 
         #endregion /* Fim Metodo Privado */;
 
 
         #region  /* Métodos Publicos */
-        public void adicionarMesa(Mesas mesa) { }
-        public void atenderCliente(Cliente cliente, int quantidadePessoas) { }
-        public void adicionarFilaEspera(Requisicao requisicao) { }
-        public void atenderProximoFilaEspera() { }
-        public void finalizarRequisicao(Requisicao requisicao) { }
+
+        
+        /// <summary>
+        /// Registra uma nova mesa para o restaurante
+        /// </summary>
+        /// <param name="mesa">Parametro para receber os dados do objeto do tipo mesa</param>
+        public void adicionarMesa(Mesa mesa) 
+        { 
+            if(_mesas.Count <= _maxMesas)
+            {
+                _mesas.Add(mesa);
+            }
+        }
+        /// <summary>
+        /// Atende a requisicao feita pelo cliente
+        /// </summary>
+        /// <param name="cliente">Recebe o cliente que fez a requisicao</param>
+        public void atenderCliente(Cliente cliente) 
+        {
+            alocarMesa(cliente.Requisicao);
+        }
+        public void adicionarFilaEspera(Requisicao requisicao) 
+        { 
+            _filaDeEspera.Enqueue(requisicao);
+        }
+        public void atenderProximoFilaEspera() 
+        { 
+            foreach(Requisicao atendimento in _filaDeEspera)
+            {
+
+            }
+        }
+        public void finalizarRequisicao(Requisicao requisicao) 
+        { 
+        
+        }
 
         #endregion /* Fim Metodo Publicos */;
 
